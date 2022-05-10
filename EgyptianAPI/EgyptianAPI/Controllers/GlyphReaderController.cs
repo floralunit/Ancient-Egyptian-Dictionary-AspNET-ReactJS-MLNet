ecacd0bf9c03275ef5ce935bb04c24069d07b28c
floralunit <71765648+floralunit@users.noreply.github.com>
2022-05-10 21:35:13 +0300

    fix api for glyphReader and create simple reader in react

--- EgyptianAPI/EgyptianAPI/Controllers/GlyphReaderController.cs
+++ EgyptianAPI/EgyptianAPI/Controllers/GlyphReaderController.cs
@@ -16,11 +16,42 @@ namespace EgyptianAPI.Controllers
     {
         // POST: api/glyphreader
         /// <summary>
-        /// Загрузка картинки и получение прогноза
+        /// РАБОЧАЯ ВЕРСИЯ Загрузка картинки и получение прогноза
         /// </summary>
         [HttpPost, Tags("Расшифратор иероглифов")]
-        public async Task<IActionResult> GetImage(IFormFile image)
+        public async Task<IActionResult> ImportFile([FromForm] IFormFile file)
         {
+            string name = file.FileName;
+            string extension = Path.GetExtension(file.FileName);
+            //read the file
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+            }
+            long length = file.Length;
+            if (length < 0)
+                return BadRequest();
+
+            using var fileStream = file.OpenReadStream();
+            byte[] imageBytes = new byte[length];
+            fileStream.Read(imageBytes, 0, (int)file.Length);
+            EgyptianMLModel.ModelInput sampleData = new EgyptianMLModel.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            //Load model and predict output
+            var result = EgyptianMLModel.Predict(sampleData);
+            return Ok(result.PredictedLabel);
+        }
+        // POST: api/glyphreader
+        /// <summary>
+        /// НЕ РАБОЧАЯ В РЕАКТЕ Загрузка картинки и получение прогноза с возможностью с загрузить файл в сваггере
+        /// </summary>
+        [HttpPost("loadimage"), Tags("Расшифратор иероглифов")]
+        public async Task<IActionResult> LoadImage(IFormFile image)
+        {
+
             //Load sample data
             long length = image.Length;
             if (length < 0)
@@ -37,8 +68,6 @@ namespace EgyptianAPI.Controllers
             //Load model and predict output
             var result = EgyptianMLModel.Predict(sampleData);
             return Ok(result.PredictedLabel);
-
         }
-
     }
 }
--- react-app/src/pages/GlyphReader.js
+++ react-app/src/pages/GlyphReader.js
@@ -1,33 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-export function GlyphReader() {
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [prediction, setPrediction] = useState("Результат расщифровки будет здесь");
+export const GlyphReader = () => {
+    const [fileSelected, setFileSelected] = useState();
+    const [prediction, setPrediction] = useState(null);
+
+    const saveFileSelected= (e) => {
+        setFileSelected(e.target.files[0]);
+    };
+
+    const importFile= async (e) => {
+        setPrediction("Загрузка...");
+        const formData = new FormData();
+        formData.append("file", fileSelected);
+        try {
+            const res = await axios.post("https://api.ancient-egyptian-helper.ru/api/glyphreader", formData);
+            setPrediction(res.data);
+        } catch (ex) {
+            console.log(ex);
+        }
+    };
 
     return (
-        <div>
-            <h1>Upload and Display Image usign React Hook's</h1>
-            {selectedImage && (
-                <div>
-                    <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-                    <br />
-                    <button onClick={()=>setSelectedImage(null)}>Remove</button>
-                </div>
-            )}
-            <br />
-
-            <br />
-            <input
-                type="file"
-                name="myImage"
-                onChange={(event) => {
-                    console.log(event.target.files[0]);
-                    setSelectedImage(event.target.files[0]);
-                }}
-            />
-            <div>
+        <div className={"glyphreader"}>
+            <input type="file" onChange={saveFileSelected} />
+            <input type="button" value="upload" onClick={importFile} />
                 {prediction}
-            </div>
         </div>
     );
-}
+};
