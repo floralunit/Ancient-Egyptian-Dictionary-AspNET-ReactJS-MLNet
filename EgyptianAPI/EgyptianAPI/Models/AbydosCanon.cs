4eefa788eec3391acef66283284c550583af055a
floralunit <71765648+floralunit@users.noreply.github.com>
2022-06-05 04:50:28 +0300

    fix id in pharaohs

--- EgyptianAPI/EgyptianAPI/Models/AbydosCanon.cs
+++ EgyptianAPI/EgyptianAPI/Models/AbydosCanon.cs
@@ -5,7 +5,7 @@ namespace EgyptianAPI.Models
 {
     public partial class AbydosCanon
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string? NameInList { get; set; }
         public string? Transliteration { get; set; }
         public string? PharaohName { get; set; }
