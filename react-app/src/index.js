d771b88c18107c963412b3d609a2fea8ee4d06a2
floralunit <71765648+floralunit@users.noreply.github.com>
2022-06-14 21:56:31 +0300

    fix warnings

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById("root")
+    </Provider>
 );
